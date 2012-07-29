c8d17a235a152233926cdd724efcd745de40dd7e
Dr. ERDI Gergo <gergo@erdi.hu>
2012-07-29 15:33:06 +0800

    kill obnoxious base monad from Infer/Fresh

--- lambdacube-core/sandbox/syntax/typing/mgu.hs
+++ lambdacube-core/sandbox/syntax/typing/mgu.hs
@@ -15,10 +15,9 @@ import Data.Traversable (mapM)
 import Data.Stream (Stream(..))
 import qualified Data.Stream as Stream
 
-import Control.Monad.Identity (Identity, runIdentity)
 import Control.Monad.Trans
-import Control.Monad.Trans.State
-import Control.Monad.Trans.Reader
+import Control.Monad.State hiding (mapM)
+import Control.Monad.Reader hiding (mapM)
 import Control.Arrow
 
 type Id = String
@@ -62,52 +61,52 @@ occurs x _ = False
 class (Monad m) => MonadFresh a m | m -> a where
     fresh :: m a
 
-newtype Fresh m a = Fresh{ unFresh :: StateT (Stream Id) m a }
-                  deriving (Functor, Applicative, Monad)
+newtype Fresh a = Fresh{ unFresh :: State (Stream Id) a }
+                deriving (Functor, Applicative, Monad)
 
-instance (Monad m) => MonadFresh Tv (Fresh m) where
+instance MonadFresh Tv Fresh where
     fresh = do
         Cons x xs <- Fresh get
         Fresh $ put xs
         return x
 
-runFresh :: Fresh Identity a -> a
+runFresh :: Fresh a -> a
 runFresh m = evalState (unFresh m) (Stream.map (('v':) . show) $ Stream.iterate succ 0)
 
-newtype Unify m a = Unify{ unUnify :: StateT Subst (Fresh m) a }
+newtype Unify a = Unify{ unUnify :: StateT Subst Fresh a }
                 deriving (Functor, Applicative, Monad)
 
-instance (Monad m) => MonadFresh Tv (Unify m) where
+instance MonadFresh Tv Unify where
     fresh = Unify $ lift fresh
 
 type Subst = Map Tv Ty
 
-substTv :: (Monad m) => Tv -> Unify m Ty
+substTv :: Tv -> Unify Ty
 substTv x = do
     mt <- Unify $ gets $ Map.lookup x
     case mt of
         Nothing -> return $ TyVar x
         Just t -> subst t
 
-subst :: (Monad m) => Ty -> Unify m Ty
+subst :: Ty -> Unify Ty
 subst t = case t of
     TyVar x -> substTv x
-    TyApp t u -> liftM2 TyApp (subst t) (subst u)
+    TyApp t u -> TyApp <$> subst t <*> subst u
     _ -> return t
 
-addSubst :: (Monad m) => Tv -> Ty -> Unify m ()
+addSubst :: Tv -> Ty -> Unify ()
 addSubst x (TyVar y) | x == y = return ()
 addSubst x t | occurs x t = fail $ unwords ["infinite type:", show x, "=", show t]
              | otherwise = Unify $ modify $ Map.insert x t
 
-unifyEqs :: (Monad m) => [TyEq] -> Unify m ()
+unifyEqs :: [TyEq] -> Unify ()
 unifyEqs tyEqs = case tyEqs of
     [] -> return ()
     (e:es) -> case unify1 e of
         Skip -> unifyEqs es
         Substitute x t -> do
             addSubst x t
-            es' <- mapM (\(t :~: t') -> liftM2 (:~:) (subst t) (subst t')) es
+            es' <- mapM (\(t :~: t') -> (:~:) <$> subst t <*> subst t') es
             unifyEqs es'
         Recurse es' -> unifyEqs $ es' ++ es
         Flip -> unifyEqs $ (flip e):es
@@ -128,12 +127,12 @@ monoVars = Map.keys . monoVarMap
 removeMonoVars :: [Var] -> MonoEnv -> MonoEnv
 removeMonoVars vars m@MonoEnv{..} = m{ monoVarMap = foldr Map.delete monoVarMap vars }
 
-substMonoEnv :: (Monad m) => MonoEnv -> Unify m MonoEnv
+substMonoEnv :: MonoEnv -> Unify MonoEnv
 substMonoEnv m = do
     monoVarMap' <- mapM subst $ monoVarMap m
     return m{ monoVarMap = monoVarMap' }
 
-unify :: (Monad m) => [MonoEnv] -> [Ty] -> Unify m (MonoEnv, Ty)
+unify :: [MonoEnv] -> [Ty] -> Unify (MonoEnv, Ty)
 unify ms τs = do
     α <- fresh
     let eqs = map (TyVar α :~:) τs
@@ -189,40 +188,40 @@ type Typing = (MonoEnv, Ty)
 type Typings = (MonoEnv, [Ty])
 newtype PolyEnv = PolyEnv{ polyEnvMap :: Map Var Typing }
 
-newtype Infer m a = Infer{ unInfer :: ReaderT (TyEnv, PolyEnv) (Fresh m) a }
-                  deriving (Functor, Applicative, Monad)
+newtype Infer a = Infer{ unInfer :: ReaderT (TyEnv, PolyEnv) Fresh a }
+                deriving (Functor, Applicative, Monad)
 
 runInfer m = curry $ runReaderT $ unInfer m
 
-instance (Monad m) => MonadFresh Tv (Infer m) where
+instance MonadFresh Tv Infer where
     fresh = Infer . lift $ fresh
 
-lookupCon :: (Monad m) => Con -> Infer m Ty
+lookupCon :: Con -> Infer Ty
 lookupCon con = do
     mτ <- Infer . asks $ Map.lookup con . tyEnvCons . fst
     case mτ of
         Nothing -> fail $ unwords ["Undefined constructor", show con]
         Just τ -> return τ
 
-lookupPolyVar :: (Monad m) => Var -> Infer m (Maybe Typing)
+lookupPolyVar :: Var -> Infer (Maybe Typing)
 lookupPolyVar x = Infer . asks $ Map.lookup x . polyEnvMap . snd
 
-shadow :: (Monad m) => MonoEnv -> Infer m a -> Infer m a
+shadow :: MonoEnv -> Infer a -> Infer a
 shadow m = Infer . local (second fixup) . unInfer
   where
     fixup env@PolyEnv{..} = env{ polyEnvMap = foldr Map.delete polyEnvMap vars }
     vars = monoVars m
 
-runUnify :: (Monad m) => Unify m a -> Infer m a
+runUnify :: Unify a -> Infer a
 runUnify u = Infer . lift $ evalStateT (unUnify u) mempty
 
-inferExpr :: (Monad m) => Expr -> Infer m Typing
+inferExpr :: Expr -> Infer Typing
 inferExpr e = case e of
     EVar x -> do
         mtyping <- lookupPolyVar x
         case mtyping of
             Nothing -> do
-                α <- liftM TyVar fresh
+                α <- TyVar <$> fresh
                 return (MonoEnv $ Map.singleton x α, α)
             Just typing -> return typing
     ECon c -> do
@@ -231,7 +230,7 @@ inferExpr e = case e of
     EApp f e -> do
         (m1, τ1) <- inferExpr f
         (m2, τ2) <- inferExpr e
-        α <- liftM TyVar fresh
+        α <- TyVar <$> fresh
         (m, TyApp (TyApp TyFun _) τ) <- runUnify $ unify [m1, m2] [τ1, TyApp (TyApp TyFun τ2) α]
         return (m, τ)
     ELam pat e -> do
@@ -241,24 +240,24 @@ inferExpr e = case e of
         let m' = removeMonoVars (monoVars mPat) m
         return (m', τ)
 
-inferPat :: (Monad m) => Pat -> Infer m Typing
+inferPat :: Pat -> Infer Typing
 inferPat pat = case pat of
     PVar x -> do
-        α <- liftM TyVar fresh
+        α <- TyVar <$> fresh
         return (MonoEnv $ Map.singleton x α, α)
     PWildcard -> do
-        α <- liftM TyVar fresh
+        α <- TyVar <$> fresh
         return (mempty, α)
     PCon con pats -> do
         τ0 <- lookupCon con
         (m, τs) <- inferPats pats
-        α <- liftM TyVar fresh
+        α <- TyVar <$> fresh
         (m', τ) <- runUnify $ unify [m] [τ0, foldr (~>) α τs]
         return (m', tyFunResult τ)
 
-inferPats :: (Monad m) => [Pat] -> Infer m Typings
+inferPats :: [Pat] -> Infer Typings
 inferPats pats = do
-    (ms, τs) <- liftM unzip $ mapM inferPat pats
+    (ms, τs) <- unzip <$> mapM inferPat pats
     -- TODO: check that the vars of each m are pairwise disjunct
     return (mconcat ms, τs)
 
