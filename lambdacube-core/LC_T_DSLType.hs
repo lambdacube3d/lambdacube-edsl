cb9a52ab72cb192d472aa19294d8f1417982702c
Csaba Hruska <csaba.hruska@gmail.com>
2013-05-06 11:24:07 +0200

    remove unused code from IsTuple class

--- lambdacube-core/LC_T_DSLType.hs
+++ lambdacube-core/LC_T_DSLType.hs
@@ -555,53 +555,33 @@ type instance EltRepr' (a, b, c, d, e, f, g, h, i) = (EltRepr (a, b, c, d, e, f,
 -- our language uses nested tuple representation
 class IsTuple tup where
     type TupleRepr tup
-    fromTuple :: tup -> TupleRepr tup
-    toTuple   :: TupleRepr tup -> tup
 
 instance IsTuple () where
     type TupleRepr () = ()
-    fromTuple         = id
-    toTuple           = id
             
 instance IsTuple (a, b) where
     type TupleRepr (a, b) = (((), a), b)
-    fromTuple (x, y)      = (((), x), y)
-    toTuple (((), x), y)  = (x, y)
             
 instance IsTuple (a, b, c) where
     type TupleRepr (a, b, c)  = (TupleRepr (a, b), c)
-    fromTuple (x, y, z)       = ((((), x), y), z)
-    toTuple ((((), x), y), z) = (x, y, z)
 
 instance IsTuple (a, b, c, d) where
     type TupleRepr (a, b, c, d)    = (TupleRepr (a, b, c), d)
-    fromTuple (x, y, z, v)         = (((((), x), y), z), v)
-    toTuple (((((), x), y), z), v) = (x, y, z, v)
 
 instance IsTuple (a, b, c, d, e) where
     type TupleRepr (a, b, c, d, e)      = (TupleRepr (a, b, c, d), e)
-    fromTuple (x, y, z, v, w)           = ((((((), x), y), z), v), w)
-    toTuple ((((((), x), y), z), v), w) = (x, y, z, v, w)
 
 instance IsTuple (a, b, c, d, e, f) where
     type TupleRepr (a, b, c, d, e, f)        = (TupleRepr (a, b, c, d, e), f)
-    fromTuple (x, y, z, v, w, r)             = (((((((), x), y), z), v), w), r)
-    toTuple (((((((), x), y), z), v), w), r) = (x, y, z, v, w, r)
 
 instance IsTuple (a, b, c, d, e, f, g) where
     type TupleRepr (a, b, c, d, e, f, g)          = (TupleRepr (a, b, c, d, e, f), g)
-    fromTuple (x, y, z, v, w, r, s)               = ((((((((), x), y), z), v), w), r), s)
-    toTuple ((((((((), x), y), z), v), w), r), s) = (x, y, z, v, w, r, s)
 
 instance IsTuple (a, b, c, d, e, f, g, h) where
     type TupleRepr (a, b, c, d, e, f, g, h)            = (TupleRepr (a, b, c, d, e, f, g), h)
-    fromTuple (x, y, z, v, w, r, s, t)                 = (((((((((), x), y), z), v), w), r), s), t)
-    toTuple (((((((((), x), y), z), v), w), r), s), t) = (x, y, z, v, w, r, s, t)
 
 instance IsTuple (a, b, c, d, e, f, g, h, i) where
     type TupleRepr (a, b, c, d, e, f, g, h, i)              = (TupleRepr (a, b, c, d, e, f, g, h), i)
-    fromTuple (x, y, z, v, w, r, s, t, u)                   = ((((((((((), x), y), z), v), w), r), s), t), u)
-    toTuple ((((((((((), x), y), z), v), w), r), s), t), u) = (x, y, z, v, w, r, s, t, u)
 
 -- Tuple representation
 -- --------------------
