dec02d9e250e92ac63296f9db0511bc9dba1037b
Elise Huard <elise@jabberwocky.eu>
2014-05-12 13:21:01 +0200

    upgrade to work with GHC-7.8

--- lambdacube-core/LC_B_GLData.hs
+++ lambdacube-core/LC_B_GLData.hs
@@ -8,6 +8,7 @@ import Data.List as L
 import Data.Maybe
 import Data.Trie as T
 import Foreign 
+import System.IO.Unsafe
 --import qualified Data.IntMap as IM
 import qualified Data.Map as Map
 import qualified Data.Set as Set
