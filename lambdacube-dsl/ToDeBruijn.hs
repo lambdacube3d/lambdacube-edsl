b3e106988537fabf34d45b77808f449995ef93f5
Csaba Hruska <csaba.hruska@gmail.com>
2015-02-28 20:20:27 +0100

    more primitive functions are converted

--- lambdacube-dsl/ToDeBruijn.hs
+++ lambdacube-dsl/ToDeBruijn.hs
@@ -60,6 +60,14 @@ data NF
   | RasterContext RasterContext
   | IV4F ByteString
   | IM44F ByteString
+  | BlendingFactor BlendingFactor
+  | BlendEquation BlendEquation
+  | LogicOperation LogicOperation
+  | StencilOperation StencilOperation
+  | ComparisonFunction ComparisonFunction
+  | FrontFace FrontFace
+  | PointSpriteCoordOrigin PointSpriteCoordOrigin
+  | Bool' Bool
   deriving (Show)
 
 instance Show N where
@@ -95,28 +103,87 @@ toNF _ _ x = error $ "toNF error: " ++ show x
 
 eval :: [NF] -> [NF]
 -- Vector/Matrix
+eval (Fun "True":xs) = Bool' True: eval xs
+eval (Fun "False":xs) = Bool' False: eval xs
 eval (Fun "V4F":Arg (LFloat x):Arg (LFloat y):Arg (LFloat z):Arg (LFloat w):xs) = Val (Single C.V4F) (VV4F (V4 (realToFrac x) (realToFrac y) (realToFrac z) (realToFrac w))) : eval xs
 -- Input declaration
 eval (Fun "Uni":IM44F a:xs) = N (uni (Single M44F) a) : eval xs
 eval (Fun "IV4F":Arg (LString s):xs) = IV4F (pack s) : eval xs
 eval (Fun "IM44F":Arg (LString s):xs) = IM44F (pack s) : eval xs
 -- BlendingFactor
+eval (Fun "Zero":xs) = BlendingFactor Zero: eval xs
+eval (Fun "One":xs) = BlendingFactor One: eval xs
+eval (Fun "SrcColor":xs) = BlendingFactor SrcColor: eval xs
+eval (Fun "OneMinusSrcColor":xs) = BlendingFactor OneMinusSrcColor: eval xs
+eval (Fun "DstColor":xs) = BlendingFactor DstColor: eval xs
+eval (Fun "OneMinusDstColor":xs) = BlendingFactor OneMinusDstColor: eval xs
+eval (Fun "SrcAlpha":xs) = BlendingFactor SrcAlpha: eval xs
+eval (Fun "OneMinusSrcAlpha":xs) = BlendingFactor OneMinusSrcAlpha: eval xs
+eval (Fun "DstAlpha":xs) = BlendingFactor DstAlpha: eval xs
+eval (Fun "OneMinusDstAlpha":xs) = BlendingFactor OneMinusDstAlpha: eval xs
+eval (Fun "ConstantColor":xs) = BlendingFactor ConstantColor: eval xs
+eval (Fun "OneMinusConstantColor":xs) = BlendingFactor OneMinusConstantColor: eval xs
+eval (Fun "ConstantAlpha":xs) = BlendingFactor ConstantAlpha: eval xs
+eval (Fun "OneMinusConstantAlpha":xs) = BlendingFactor OneMinusConstantAlpha: eval xs
+eval (Fun "SrcAlphaSaturate":xs) = BlendingFactor SrcAlphaSaturate: eval xs
 -- BlendEquation
+eval (Fun "FuncAdd":xs) = BlendEquation FuncAdd: eval xs
+eval (Fun "FuncSubtract":xs) = BlendEquation FuncSubtract: eval xs
+eval (Fun "FuncReverseSubtract":xs) = BlendEquation FuncReverseSubtract: eval xs
+eval (Fun "Min":xs) = BlendEquation Min: eval xs
+eval (Fun "Max":xs) = BlendEquation Max: eval xs
 -- LogicOperation
+eval (Fun "Clear":xs) = LogicOperation Clear: eval xs
+eval (Fun "And":xs) = LogicOperation And: eval xs
+eval (Fun "AndReverse":xs) = LogicOperation AndReverse: eval xs
+eval (Fun "Copy":xs) = LogicOperation Copy: eval xs
+eval (Fun "AndInverted":xs) = LogicOperation AndInverted: eval xs
+eval (Fun "Noop":xs) = LogicOperation Noop: eval xs
+eval (Fun "Xor":xs) = LogicOperation Xor: eval xs
+eval (Fun "Or":xs) = LogicOperation Or: eval xs
+eval (Fun "Nor":xs) = LogicOperation Nor: eval xs
+eval (Fun "Equiv":xs) = LogicOperation Equiv: eval xs
+eval (Fun "Invert":xs) = LogicOperation Invert: eval xs
+eval (Fun "OrReverse":xs) = LogicOperation OrReverse: eval xs
+eval (Fun "CopyInverted":xs) = LogicOperation CopyInverted: eval xs
+eval (Fun "OrInverted":xs) = LogicOperation OrInverted: eval xs
+eval (Fun "Nand":xs) = LogicOperation Nand: eval xs
+eval (Fun "Set":xs) = LogicOperation Set: eval xs
 -- StencilOperation
+eval (Fun "OpZero":xs) = StencilOperation OpZero: eval xs
+eval (Fun "OpKeep":xs) = StencilOperation OpKeep: eval xs
+eval (Fun "OpReplace":xs) = StencilOperation OpReplace: eval xs
+eval (Fun "OpIncr":xs) = StencilOperation OpIncr: eval xs
+eval (Fun "OpIncrWrap":xs) = StencilOperation OpIncrWrap: eval xs
+eval (Fun "OpDecr":xs) = StencilOperation OpDecr: eval xs
+eval (Fun "OpDecrWrap":xs) = StencilOperation OpDecrWrap: eval xs
+eval (Fun "OpInvert":xs) = StencilOperation OpInvert: eval xs
 -- ComparisonFunction
+eval (Fun "Never":xs) = ComparisonFunction Never: eval xs
+eval (Fun "Less":xs) = ComparisonFunction Less: eval xs
+eval (Fun "Equal":xs) = ComparisonFunction Equal: eval xs
+eval (Fun "Lequal":xs) = ComparisonFunction Lequal: eval xs
+eval (Fun "Greater":xs) = ComparisonFunction Greater: eval xs
+eval (Fun "Notequal":xs) = ComparisonFunction Notequal: eval xs
+eval (Fun "Gequal":xs) = ComparisonFunction Gequal: eval xs
+eval (Fun "Always":xs) = ComparisonFunction Always: eval xs
 -- ProvokingVertex
 eval (Fun "LastVertex":xs) = ProvokingVertex LastVertex : eval xs
 eval (Fun "FirstVertex":xs) = ProvokingVertex FirstVertex : eval xs
 -- CullMode
 eval (Fun "CullNone":xs) = CullMode CullNone : eval xs
+eval (Fun "CullFront":FrontFace f:xs) = CullMode (CullFront f) : eval xs
+eval (Fun "CullBack":FrontFace f:xs) = CullMode (CullBack f) : eval xs
 -- FrontFace
+eval (Fun "CW":xs) = FrontFace CW: eval xs
+eval (Fun "CCW":xs) = FrontFace CCW: eval xs
 -- PolygonMode
 eval (Fun "PolygonFill":xs) = PolygonMode PolygonFill : eval xs
 eval (Fun "PolygonLine":Arg (LFloat s):xs) = PolygonMode (PolygonLine $ realToFrac s) : eval xs
 eval (Fun "PolygonPoint":PointSize' s:xs) = PolygonMode (PolygonPoint s) : eval xs
 -- PolygonOffset
 eval (Fun "NoOffset":xs) = PolygonOffset NoOffset : eval xs
+eval (Fun "Offset":Arg (LFloat a):Arg (LFloat b):xs) = PolygonOffset (Offset (realToFrac a) (realToFrac b)): eval xs
 -- PointSize
 eval (Fun "PointSize":Arg (LFloat s):xs) = PointSize' (PointSize $ realToFrac s) : eval xs
 eval (Fun "ProgramPointSize":xs) = PointSize' ProgramPointSize : eval xs
@@ -126,24 +193,40 @@ eval (Fun "FragmentOut":N a:xs) = N (fragmentOut [a]) : eval xs
 -- Vertex Out
 eval (Fun "VertexOut":N a:Arg (LFloat b):N c:N d:xs) = N (vertexOut a (const_ (Single C.Float) (VFloat $ realToFrac b)) [c] [d]) : eval xs
 -- PointSpriteCoordOrigin
+eval (Fun "LowerLeft":xs) = PointSpriteCoordOrigin LowerLeft: eval xs
+eval (Fun "UpperLeft":xs) = PointSpriteCoordOrigin UpperLeft: eval xs
 -- Raster Context
 eval (Fun "TriangleCtx":CullMode a:PolygonMode b:PolygonOffset c:ProvokingVertex d:xs) = RasterContext (TriangleCtx a b c d) : eval xs
+eval (Fun "PointCtx":PointSize' a: Arg (LFloat b):PointSpriteCoordOrigin c:xs) = RasterContext (PointCtx a (realToFrac b) c) : eval xs
+eval (Fun "LineCtx":Arg (LFloat a):ProvokingVertex b:xs) = RasterContext (LineCtx (realToFrac a) b) : eval xs
 -- Fetch Primitive
+eval (Fun "Points":xs) = FetchPrimitive Points: eval xs
+eval (Fun "Lines":xs) = FetchPrimitive Lines: eval xs
 eval (Fun "Triangles":xs) = FetchPrimitive Triangles : eval xs
+eval (Fun "LinesAdjacency":xs) = FetchPrimitive LinesAdjacency: eval xs
+eval (Fun "TrianglesAdjacency":xs) = FetchPrimitive TrianglesAdjacency: eval xs
 -- Accumulation Context
 eval (Fun "AccumulationContext":FragmentOperation a:xs) = N (accumulationContext Nothing [a]) : eval xs
 -- Image
 eval (Fun "ColorImage":Arg (LNat i):Val _ v:xs) = Img (ColorImage i v) : eval xs
+--TODO  "DepthImage"   -> do [a] <- newVars 1 C ; ty $ a ~> TFloat C ~> TImage C a (TFloat C)
+--TODO  "StencilImage" -> do [a] <- newVars 1 C ; ty $ a ~> TInt C ~> TImage C a (TInt C)
 -- Interpolation
 eval (Fun "Smooth":N a:xs) = N (smooth a) : eval xs
 eval (Fun "NoPerspective":N a:xs) = N (noPerspective a) : eval xs
 eval (Fun "Flat":N a:xs) = N (flat a) : eval xs
 -- Fragment Operation
 eval (Fun "ColorOp":Blending b:xs) = FragmentOperation (ColorOp b (VV4B $ V4 True True True True)) : eval xs
+eval (Fun "DepthOp":ComparisonFunction a:Bool' b:xs) = FragmentOperation (DepthOp a b) : eval xs
 -- Blending
 eval (Fun "NoBlending":xs) = Blending NoBlending : eval xs
+--TODO  "BlendLogicOp" -> do t <- newVar C ; ty $ TLogicOperation C ~> TBlending C t
+--TODO  "Blend"        -> ty $ TTuple C [TBlendEquation C,TBlendEquation C]
+--                     ~> TTuple C [TTuple C [TBlendingFactor C,TBlendingFactor C],TTuple C [TBlendingFactor C,TBlendingFactor C]]
+--                     ~> TV4F C ~> TBlending C (TFloat C)
 -- Fragment Filter
 eval (Fun "PassAll":xs) = N passAll : eval xs
+eval (Fun "Filter":N f:xs) = N (filter_ f) : eval xs
 -- Render Operations
 eval (Fun "Fetch":Arg (LString a):FetchPrimitive b:IV4F c:xs) = N (fetch (pack a) b [(c,V4F)]) : eval xs
 eval (Fun "Transform":N a:N b:xs) = N (transform a b) : eval xs
